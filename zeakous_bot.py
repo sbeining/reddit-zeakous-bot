5c6d6e6aafae3d647be56cda9b2a6490e2008fbb
Sascha Beining <beining@saltation.de>
2017-07-20 22:49:34 +0200

    Reply to the "zealous" comments with the ZEAKOUS meme

--- zeakous_bot.py
+++ zeakous_bot.py
@@ -16,12 +16,17 @@ else:
        comments_replied_to = comments_replied_to.split("\n")
        comments_replied_to = list(filter(None, comments_replied_to))
 
+pattern = re.compile("zealous", re.IGNORECASE)
+
 for submission in subreddit.hot(limit=10):
     submission.comments.replace_more(limit=0)
     for comment in submission.comments.list():
-        if re.search("zealous", comment.body, re.IGNORECASE):
-            print(comment.body)
-            comments_replied_to.append(comment.id)
+        if comment.id not in comments_replied_to:
+            for line in comment.body.split("\n"):
+                if re.search("zealous", line, re.IGNORECASE):
+                    replyBody = "> " + pattern.sub("**ZEAKOUS**", line) + "\n\n" + "\"I've been memed again!\" -/u/lbibass "
+                    comments_replied_to.append(comment.id)
+                    comment.reply(replyBody)
 
 # Write our updated list back to the file
 with open("comments_replied_to.txt", "w") as f:
