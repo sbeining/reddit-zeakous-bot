14437715f1092781e2d49ea5dfd89bd6b71dfc5a
Sascha Beining <beining@saltation.de>
2017-07-20 23:32:07 +0200

    Cleanup the proof of concept
    The code is separated into functions and comments were provided for code whoose
    purpose is not immediately clear

--- zeakous_bot.py
+++ zeakous_bot.py
@@ -4,31 +4,65 @@ import praw
 import re
 import os
 
-reddit = praw.Reddit('bot1')
+def main():
+    global comments_replied_to
 
-subreddit = reddit.subreddit("longboarding")
+    reddit = praw.Reddit('bot1')
+    subreddit = reddit.subreddit("longboarding")
+    comments_replied_to = load_comments_replied_to()
 
-if not os.path.isfile("comments_replied_to.txt"):
-    comments_replied_to = []
-else:
-    with open("comments_replied_to.txt", "r") as f:
-       comments_replied_to = f.read()
-       comments_replied_to = comments_replied_to.split("\n")
-       comments_replied_to = list(filter(None, comments_replied_to))
+    for submission in subreddit.hot(limit=10):
+        process_submission(submission)
 
-pattern = re.compile("zealous", re.IGNORECASE)
+    save_comments_replied_to(comments_replied_to)
 
-for submission in subreddit.hot(limit=10):
+
+def process_submission(submission):
+    # Expand all "load more comments" entries
     submission.comments.replace_more(limit=0)
-    for comment in submission.comments.list():
-        if comment.id not in comments_replied_to:
-            for line in comment.body.split("\n"):
-                if re.search("zealous", line, re.IGNORECASE):
-                    replyBody = "> " + pattern.sub("**ZEAKOUS**", line) + "\n\n" + "\"I've been memed again!\" -/u/lbibass "
-                    comments_replied_to.append(comment.id)
-                    comment.reply(replyBody)
 
-# Write our updated list back to the file
-with open("comments_replied_to.txt", "w") as f:
-    for comment_id in comments_replied_to:
-        f.write(comment_id + "\n")
+    # Flatten top level comments an replies
+    comments = submission.comments.list()
+
+    for comment in comments:
+        # Skip if already replied
+        if comment.id in comments_replied_to:
+            continue
+
+        # Skip if the comment does not contain the magic word
+        if not re.search("zealous", comment.body, re.IGNORECASE):
+            continue
+
+        process_comment(comment)
+        comments_replied_to.append(comment.id)
+
+def process_comment(comment):
+    pattern = re.compile("zealous", re.IGNORECASE)
+
+    # Handle lines individually to not quote too much
+    for line in comment.body.split("\n"):
+        if not re.search("zealous", line, re.IGNORECASE):
+            continue
+
+        replyBody = "> " + pattern.sub("**ZEAKOUS**", line) + "\n\n" + "\"I've been memed again!\" -/u/lbibass "
+
+        comment.reply(replyBody)
+
+def load_comments_replied_to():
+    if not os.path.isfile("comments_replied_to.txt"):
+        result = []
+    else:
+        with open("comments_replied_to.txt", "r") as f:
+           result = f.read()
+           result = result.split("\n")
+           result = list(filter(None, result))
+
+    return result
+
+def save_comments_replied_to(comment_ids):
+    with open("comments_replied_to.txt", "w") as f:
+        for comment_id in comment_ids:
+            f.write(comment_id + "\n")
+
+if __name__ == '__main__':
+    main()
